101ea55b227d55dc35deda7c9677e27adb0bb108
sacanthias <nob6010@rit.edu>
2025-11-22 01:55:53 -0500

    project 2 done

--- project_2/js/main.js
+++ project_2/js/main.js
@@ -1,87 +1,100 @@
 const apiPath = "https://api.jikan.moe/v4";
+const prefix = "nob6010";
+const listKey = prefix + "list";
+const searchKey = prefix + "search";
+const searchTerm = localStorage.getItem(searchKey);
+const userSaved = localStorage.getItem(listKey);
+
 let sfw;
 let limit;
 let anime;
 let manga;
-let userList;
-let saveButtons;
+let userList = new Array();
+let saveButtons = [];
+let callResult;
 
-window.onload = (e) => {
+window.onload = function(){
     populateHome();
     document.querySelector("#search").onclick = searchClicked;
+    document.querySelector("#viewList").onclick = listClicked;
 };
 
 function populateHome(){
-    let topAnimeURL = apiPath + "/top/anime";
-    console.log(topAnimeURL);
+    // seasonal anime
+    let seasonalURL = apiPath + "/seasons/now?limit=7";
+    getData(seasonalURL, "#seasonal", "aniseason");
 
-    //getData(topAnimeURL);
-}
+    // top manga
+    let topMangaURL = apiPath + "/top/manga?limit=7";
+    getData(topMangaURL, "#top_manga", "mantop")
 
-// TODO: solve the timing issue below.
-// somehow make the dataLoaded function wait for the xhr GET or something
-// use await xhr.open()?
-// but i want to keep things separated as they are now without needing to encapsulate other functions....
-async function dataLoaded(){
-    console.log("data has loaded")
-    let xhr = this;
-    console.log(xhr.responseText);
+    // top anime
+    let topAnimeURL = apiPath + "/top/anime?limit=7";
+    getData(topAnimeURL, "#top_anime", "anitop");
 
-    if(!xhr.responseText){
-        console.log("response text was undefined");
-        return Promise.reject(new Error("rejected!"));;
+    if(searchTerm){
+        document.querySelector("#searchterm").value = searchTerm;
     }
 
-    let obj = JSON.parse(xhr.responseText);
-
-    if(!obj.data || obj.data.length == 0){
-        //document.querySelector("#status").innerHTML = "<b>No results found for '" + displayTerm + "'</b>";
-        console.log("no results found.");
-        return; //bail out
+    if(userSaved){
+        userList = JSON.parse(localStorage.getItem(listKey));
+        
+        listClicked();
     }
-
-    return obj.data;
+    else{
+        document.querySelector("#showList").innerHTML = "<b>No saved entries found.</b>";
+    }
 }
 
-async function dataDisplay(containerID, classN){
-    console.log("calling");
-    let callResult = await dataLoaded();
-    console.log("received data");
-    console.log("call result:" + callResult);
+// listClicked handles displaying the user's saved list (if found)
+function listClicked(){
+    document.querySelector(".user.load-home").style.display = "block";
 
-    if(callResult){
+    if(userSaved){
+        let list = JSON.parse(localStorage.getItem(listKey));
         let bigString = "";
 
-        for (let result of callResult){
-
-            let image = result.images.jpg.image_url;
-            if(!image) image = "images/no-image-found.png";
-
-            let url = result.url;
-            let rating = result.rating;
-
-            let line = `<div class='${classN}'><img src='${smallURL}' title='${result.id}' />`;
-            line += `<span><a target ='_blank' href='${url}'>View on MAL</a><br>Rating: ${rating}</span></div>`;
-
+        for(let s of list){
+            let line = `<div class='saved'><img src='${s.image}' />`;
+            line += `<span><a target ='_blank' href='${s.url}'>${s.title}</a>
+                <br>Rating: ${s.rating}</span>
+                </div>`;
             bigString += line;
         }
 
-        document.querySelector(containerID).innerHTML = bigString;
+        document.querySelector("#showList").innerHTML = bigString;
+    }
+    else{
+        document.querySelector("#showList").innerHTML = "<b>No saved entries found.</b>";
     }
 }
 
-// searchClicked is to *specifically* handle searching 
+// searchClicked handles data requests & displays once the search button is clicked
 function searchClicked(){
     console.log("search button was clicked!");
+    // hiding everything
+    let home = document.querySelectorAll("div.load-home");
+    for(let c of home){
+        c.style.display = "none";
+    }
+    document.querySelector("#resultText").innerHTML = "";
+    
+    // setting up the api link
     let searchURL = apiPath;
-    let term = encodeURIComponent(document.querySelector("#searchterm").value.trim());
+    let term;
+
+    if(searchTerm){
+        term = searchTerm;
+    }
+    term = encodeURIComponent(document.querySelector("#searchterm").value.trim());
+    localStorage.setItem(searchKey, document.querySelector("#searchterm").value);
+
     let limit = document.querySelector("#limit").value;
     
     sfw = document.querySelector("#mediaSFW:checked");
     anime = document.querySelector("#mediaAnime:checked");
     manga = document.querySelector("#mediaManga:checked");
 
-    // TODO: simplify this if else block.
     if(sfw){
         if(anime){
             searchURL += "/anime";
@@ -122,45 +135,112 @@ function searchClicked(){
         }
     }
 
-    
     searchURL += "&limit=" + limit;
     
     console.log(searchURL);
-    getSearch(searchURL);
-    //dataDisplay("searchResults", "results")
+    getData(searchURL, "#searchGrid", "results");
+    saveButtons = document.querySelectorAll("button.save");
+    document.querySelector("#resultTitle").innerHTML = "Results for '" + term + "'";
 }
 
-// xml request code stuff *specifically* for handling user-inputted search
-function getSearch(url){
+// TODO: solve the timing issue below.
+// somehow make the dataLoaded function wait for the xhr GET or something
+// use await xhr.open()?
+// but i want to keep things separated as they are now without needing to encapsulate other functions....
+function dataLoaded(xml){
+    console.log("data has loaded")
+    let xhr = xml;
+    //console.log(xhr.responseText);
+
+    if(!xhr.responseText){
+        console.log("response text was undefined");
+        return Promise.reject(new Error("rejected!"));;
+    }
+
+    let obj = JSON.parse(xhr.responseText);
+
+    if(!obj.data || obj.data.length == 0){
+        //document.querySelector("#status").innerHTML = "<b>No results found for '" + displayTerm + "'</b>";
+        console.log("no results found.");
+        return; //bail out
+    }
+
+    callResult = obj.data;
+    //console.log("call result:" + callResult);
+}
+
+// this is done DO NOT TOUCH
+function dataDisplay(containerID, classN){
+    if(callResult){
+        let bigString = "";
+
+        for (let result of callResult){
+
+            let image = result.images.jpg.image_url;
+            if(!image) image = "images/no-image-found.png";
+
+            let line = `<div class='${classN}'><img src='${image}' />`;
+            line += `<span><a target ='_blank' href='${result.url}'>${result.title}</a>
+                <br>Rating: ${result.score}</span>
+                <button type='button' class='save' data-name='${result.title}' data-image='${image}' data-rating='${result.score}' data-url='${result.url}'>Save</button>
+                </div>`;
+
+            bigString += line;
+        }
+
+        document.querySelector(containerID).innerHTML = bigString;
+    }
+    else{
+        console.log("callResult was undefined");
+        return;
+    }
+
+    saveButtons = document.querySelectorAll("button.save");
+    
+
+    for(let b of saveButtons){
+        b.onclick = function(e){
+            console.log(userList);
+            userList.push({
+                title: e.target.dataset.name,
+                image: e.target.dataset.image, 
+                rating: e.target.dataset.rating,
+                url: e.target.dataset.url
+            });
+
+            localStorage.setItem(listKey, JSON.stringify(userList));
+        }
+    }
+}
+
+// xml request code stuff
+async function getData(url, containerID, classN){
     // 1 - create a new XHR object
     let xhr = new XMLHttpRequest();
     
     // 4 - open connection & send request
     xhr.open("GET", url, true);
-    console.log("request opened");
+    //console.log("request opened");
     xhr.send();
+    
+    await delay(500);
 
-    // code snatched from mozilla docs dattebayo
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        const status = xhr.status;
-        console.log("xhr status: " + xhr.status);
-        if (status === 0 || (status >= 200 && status < 400)) {
-            // The request has been completed successfully
-            console.log(xhr.responseText);
-            dataLoaded;
-        } else {
-        // Oh no! There has been an error with the request!
-        }
-    }
+    dataLoaded(xhr);
+    dataDisplay(containerID, classN);
+}
+
+// delay helper method
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function dataError(e){
     console.log("ERROR");
 }
 
-saveButtons = document.querySelectorAll("button.save");
-for(let b of saveButtons){
-    b.onchange = function(e){
-        
+// automatically searches under new criteria if the search filters have been changed
+for(let checks in document.querySelectorAll(".widget").children){
+    checks.onchange = function(){
+        searchClicked();
     }
 }
